f0e541972e20955d86df8f9990df560a22d0b959
Mohamed Essam <mohammedessamomar@gmail.com>
2022-08-20 23:28:42 +0200

    Add files via upload

---
+++ Week0/Artithmatic Operations.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"I will be:"<<26+13<<endl;
+    cout<<"Every room will have:"<<20/4<<"Guests"<<endl;
+    cout<<"Item after 14% taxes:"<<1000+1000*0.14<<endl;
+    cout<<"My age is:"<<2022-1996<<endl;
+    return 0;
+
+}

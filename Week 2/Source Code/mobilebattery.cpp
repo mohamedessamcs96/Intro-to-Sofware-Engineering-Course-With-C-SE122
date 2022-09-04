15f371281f0a8c40302dd1a9dba8df6d1c1b0dbd
Mohamed Essam <mohammedessamomar@gmail.com>
2022-09-04 11:04:10 +0200

    Add files via upload

---
+++ Week 2/Source Code/mobilebattery.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+
+int main(){
+
+    int battery=98;
+
+    while(battery<=100){
+        
+        cout<<battery<<endl;
+        battery+=2;
+
+    }
+
+    
+    return 0;
+}

980d362b4aef2756469ed18fa0742bcdc16bcf4d
Mohamed Essam <mohammedessamomar@gmail.com>
2022-09-04 11:02:56 +0200

    Add files via upload

---
+++ Week 2/Source Code/blinkprintcounter.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+
+int main(){
+
+
+    int counter =30;
+    while(counter>0){
+        cout<<counter<<endl;
+        counter--;
+        //counter=counter+1;
+    }
+  
+
+
+    return 0;
+}
